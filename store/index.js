cf081bb16b406286fd3f976ee2a0654ac0aaf882
fukunagaa <k.k.0627ku@gmail.com>
2020-08-03 14:11:38 +0900

    Clipの追加 Reduxでstoreに登録し取得表示まで

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore } from "redux";
 import userReducer from "./reducers/user";
 import { composeWithDevTools } from "redux-devtools-extension";
 
